10faa208af7d3db937c2eef226e90920f364fe89
Abdhulbasith <abdhulbasith456@gmail.com>
2024-02-09 18:02:41 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -8,6 +12,9 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Skills/>
+      <Work/>
+      <Contact/>
     </div>
      
     
